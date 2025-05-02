385e5c83c223d88527c963c285c7bcb2408f25e7
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-05-02 19:55:25 +0800

    `Update images and import statements in components/TravelersSection.tsx and components/WhyUseSection.tsx`

--- public/index.ts
+++ public/index.ts
@@ -3,5 +3,14 @@ import how_it_works from "./images/how_it_works.png"
 import letai from "./images/letai.png"
 import set from "./images/set.png"
 import explore from "./images/explore.png"
+import personalized from "./images/personalized.png"
+import local from "./images/local.png"
+import traffic from "./images/traffic.png"
+import solo_travel from "./images/solo_travel.png"
+import family from "./images/family.png"
+import couples from "./images/couples.png"
+import barkadas from "./images/barkadas.png"
 
-export { baguio_panorama, how_it_works, letai, set, explore }
+export { baguio_panorama, how_it_works, letai, 
+    set, explore, local, 
+    personalized, traffic, solo_travel, family, couples, barkadas }
