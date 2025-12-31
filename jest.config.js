aa4fd211f9758e81de016728720f75d0c2c21cb9
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-12-31 23:17:55 +0800

    refactor: update Jest configuration for improved test matching and coverage collection

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,40 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/*.(test|spec).+(ts|tsx|js)'
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
+    '**/specs/**/?(*.)+(spec|test).[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx'
-      }
-    }]
-  },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
-    '!src/**/*.test.{ts,tsx}',
-    '!src/**/__tests__/**'
+    '!src/**/__tests__/*',
+    '!src/**/__mocks__/*',
+    '!src/types/**/*',
+    '!src/components/ui/**/*', // Exclude shadcn/ui components
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testTimeout: 30000,
-  extensionsToTreatAsEsm: ['.ts', '.tsx']
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '/.swc/',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
