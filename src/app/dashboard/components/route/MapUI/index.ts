e99c79e3ffc2a254f721d7d8da300454869132d3
Doniele Arys Antonio <arysantonio363@gmail.com>
2026-08-10 16:20:54 +0800

    refactor: remove MapUI component and related exports

--- src/app/dashboard/components/route/MapUI/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MapStyleSelector } from './MapStyleSelector';
-export { default as RouteSelectionPanel } from './RouteSelectionPanel';
-export { default as TrafficLegend } from './TrafficLegend';
